016819a72bf546a00fd7b0e9222205b483d33733
kampotmalinoviy <LerkaKisel@mail.ru>
2025-05-21 19:53:17 +0300

    Update generate_data.py

--- lab5_ML/generate_data.py
+++ lab5_ML/generate_data.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 np.random.seed(42)
+N = 100
+X = np.random.randint(0, 2, size=(N, 12))
 
-X = np.random.randint(0, 2, size=(100, 12))  # 100 примеров, 12 бинарных признаков
-Y = np.random.randint(0, 2, size=(100, 2))  # 100 примеров, 2 класса (one-hot encoding)
+threshold = 6
+Y = np.zeros((N, 2), dtype=int)
+for i, sample in enumerate(X):
+    if sample.sum() >= threshold:
+        Y[i] = [1, 0]  
+    else:
+        Y[i] = [0, 1]  
 
 np.savetxt('dataIn.txt', X, fmt='%d')
 np.savetxt('dataOut.txt', Y, fmt='%d')
